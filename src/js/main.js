9e01926a01502e5ddf5b90f65ee4d8d6d3b08cc7
VILIAN <denk938@gmail.com>
2023-12-13 13:17:44 +0400

    new file:   src/fonts/TTLakes-Bold.eot 	new file:   src/fonts/TTLakes-Bold.ttf 	new file:   src/fonts/TTLakes-Bold.woff 	new file:   src/fonts/TTLakes-Medium.eot 	new file:   src/fonts/TTLakes-Medium.ttf 	new file:   src/fonts/TTLakes-Medium.woff 	new file:   src/fonts/TTLakes-Regular.eot 	new file:   src/fonts/TTLakes-Regular.ttf 	new file:   src/fonts/TTLakes-Regular.woff 	deleted:    src/fonts/comic.ttf 	new file:   src/fonts/demo.html 	new file:   src/fonts/stylesheet.css 	new file:   src/img/Group.svg 	new file:   src/img/burger.svg 	new file:   src/img/burger_close.svg 	new file:   src/img/call.svg 	new file:   src/img/chat.svg 	new file:   src/img/checkstatus.svg 	new file:   src/img/divider.svg 	new file:   src/img/expand.svg 	new file:   src/img/highlight.png 	new file:   src/img/main_img.svg 	new file:   src/img/profile.svg 	new file:   src/img/repair.svg 	new file:   src/img/search.svg 	new file:   src/img/swiper_logo/acer.svg 	new file:   src/img/swiper_logo/apple.svg 	new file:   src/img/swiper_logo/bosch.svg 	new file:   src/img/swiper_logo/hp.svg 	new file:   src/img/swiper_logo/lenovo.svg 	new file:   src/img/swiper_logo/next.svg 	new file:   src/img/swiper_logo/sony.svg 	new file:   src/img/swiper_logo/sumsung.svg 	new file:   src/img/swiper_logo/viewsonic.svg 	deleted:    src/img/webpack.svg 	modified:   src/index.html 	deleted:    src/js/index.js 	new file:   src/js/main.js 	renamed:    src/fonts/.gitkeep -> src/scss/base/_base.scss 	new file:   src/scss/base/_font.scss 	new file:   src/scss/base/_reset.scss 	new file:   src/scss/base/_vars.scss 	new file:   src/scss/main.scss 	deleted:    src/scss/style.scss

---
+++ src/js/main.js
@@ -0,0 +1,68 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+  const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
+    let swiper
+
+breakpoint = window.matchMedia(breakpoint)
+
+const enableSwiper = function (className, settings) {
+  swiper = new Swiper(className, settings)
+}
+
+const checker = function () {
+  if (breakpoint.matches) {
+    return enableSwiper(swiperClass, swiperSettings)
+  } else {
+    if (swiper !== undefined) swiper.destroy(true, true)
+    return
+  }
+}
+
+breakpoint.addEventListener('change', checker)
+checker()
+  }
+
+  resizableSwiper('(max-width: 767.5px)', '.image-slider', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  mousewheel: {
+      sensitivity: 1,
+      eventsTarget:".image-slider"
+  },
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  autoHeight: true,
+  })
+})
+const btn = document.querySelector(".show-button");
+const cards = document.querySelector(".image-slider__wrapper");
+
+btn.addEventListener('click', function () {
+  if (btn.innerHTML === "Показать всё") {
+    btn.innerHTML = "Скрыть";
+    cards.style.height = getComputedStyle(cards).height; 
+    cards.style.height = 'auto'; 
+    var fullHeight = getComputedStyle(cards).height; 
+    cards.style.height = ''; 
+    setTimeout(function() {
+      cards.style.height = fullHeight;
+    }, 0);
+    const style = document.createElement('style');
+    style.innerHTML = '.show-button::before { transform: rotate(180deg); }';
+    document.head.appendChild(style);
+  } else {
+    btn.innerHTML = "Показать всё";
+    var fullHeight = getComputedStyle(cards).height; 
+    cards.style.height = fullHeight;
+    setTimeout(function() {
+      cards.style.height = '160px';
+    }, 0);
+
+    const style = document.querySelector('style');
+    if (style) {
+      document.head.removeChild(style);
+    }
+  }
+});
