d94a982ff5e9e807961f1873746bfd8de15bcc26
VILIAN <denk938@gmail.com>
2023-12-18 20:31:05 +0400

    modified:   src/index.html 	modified:   src/js/index.js 	modified:   src/scss/base/_base.scss 	modified:   src/scss/blocks/_aside.scss 	new file:   src/scss/blocks/_formChat.scss 	new file:   src/scss/blocks/_formPhone.scss 	modified:   src/scss/blocks/_header.scss 	modified:   src/scss/blocks/_slider.scss 	modified:   src/scss/main.scss

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ import './../scss/main.scss';
 import Swiper from '../../node_modules/swiper/swiper-bundle';
 
 // включение выключение свайпера
-
 window.addEventListener('DOMContentLoaded', () => {
   const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
     let swiper
@@ -69,26 +68,97 @@ checker()
   })
 })
 
-
-
-// Показать бургер меню
+// переменные для модалок
 const burger = document.querySelector(".burger-menu");
 const burgerBtnShow = document.querySelector(".burger");
 const burgerBtnClose = document.querySelector(".button-close--burger");
-const opacityBg = document.getElementById('div_aside');
+const opacityBg = document.getElementById('div-aside-burger');
+const body = document.querySelector("body");
 
+const asidePhone = document.querySelector(".aside-phone");
+const phoneBtnShow1 = document.getElementById('phone-button2');
+const phoneBtnShow2 = document.getElementById('phone-button1');
+const phoneBtnClose = document.querySelector(".aside-phone__close-btn");
+const opacityBgPh = document.getElementById('div-aside-phone');
+
+const asideChat = document.querySelector(".aside-chat");
+const chatBtnShow1 = document.getElementById('chat-button2');
+const chatBtnShow2 = document.getElementById('chat-button1');
+const chatBtnClose = document.querySelector(".aside-chat__close-btn");
+const opacityBgCh = document.getElementById('div-aside-chat');
+
+function hideShowBurger(){
+  burger.classList.toggle('burger-menu--click');
+  opacityBg.classList.toggle('opacity-bg');
+  body.classList.toggle('no-scroll');
+}
+function hideShowPhone(){
+  asidePhone.classList.toggle('aside-phone--click');
+  opacityBgPh.classList.toggle('opacity-bg');
+  body.classList.toggle('no-scroll');
+}
+function hideShowChat(){
+  asideChat.classList.toggle('aside-chat--click');
+  opacityBgCh.classList.toggle('opacity-bg');
+  body.classList.toggle('no-scroll');
+}
+document.onkeydown = function (e) {
+  if(e.key === "Escape"){
+    if(asidePhone.classList.contains('aside-phone--click')){
+      asidePhone.classList.remove('aside-phone--click');
+      opacityBgPh.classList.remove('opacity-bg');
+      body.classList.remove('no-scroll');
+    }else if(asideChat.classList.contains('aside-chat--click')){
+      asideChat.classList.remove('aside-chat--click');
+      opacityBgCh.classList.remove('opacity-bg');
+      body.classList.remove('no-scroll');
+    }
+  };
+}
+
+// Показать бургер меню
 burgerBtnShow.addEventListener('click', function(){
-  burger.classList.toggle('burger-menu--click');
-  opacityBg.classList.toggle('opacity-bg');
+  hideShowBurger();
 })
-
+opacityBg.addEventListener('click', function(){
+  hideShowBurger();
+})
 burgerBtnClose.addEventListener('click', function(){
-  burger.classList.toggle('burger-menu--click');
-  opacityBg.classList.toggle('opacity-bg');
+  hideShowBurger();
 })
-  
-// показать всё в слайдере ремонт техники различных брендов
 
+// Показать форму для телефона
+phoneBtnShow1.addEventListener('click', function(){
+  hideShowPhone()
+})
+phoneBtnShow2.addEventListener('click', function(e){
+  hideShowPhone()
+  if (burger.classList.contains('burger-menu--click')) hideShowBurger(); 
+})
+phoneBtnClose.addEventListener('click', function(){
+  hideShowPhone()
+})
+opacityBgPh.addEventListener('click', function(){
+  hideShowPhone()
+})
+
+// Показать форму для чата
+chatBtnShow1.addEventListener('click', function(){
+  hideShowChat()
+  if (asidePhone.classList.contains('burger-menu--click'))  hideShowBurger()
+})
+chatBtnShow2.addEventListener('click', function(){
+  hideShowChat()
+  if (burger.classList.contains('burger-menu--click'))  hideShowBurger()
+})
+chatBtnClose.addEventListener('click', function(){
+  hideShowChat()
+})
+opacityBgCh.addEventListener('click', function(){
+  hideShowChat()
+})
+
+// показать всё в слайдере ремонт техники различных брендов
 const btn = document.querySelector(".show-button");
 const cards = document.querySelector(".image-slider__wrapper");
 
@@ -121,7 +191,6 @@ btn2.addEventListener('click', function () {
 });
 
 //  читать далее 
-
 const section_btn = document.querySelector('.section__button');
 const sectionP = document.querySelector(".section__p");
 
@@ -136,3 +205,4 @@ section_btn.addEventListener('click', function () {
   } 
   sectionP.style.maxHeight = sectionP.classList.contains("section__p-active") ? `${sectionP.scrollHeight}px` : "160px";
 });
+
